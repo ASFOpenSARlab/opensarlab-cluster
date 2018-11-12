3b0603abcdb9a56489f6012d18f706ec42b53bdd
mnamneus <mnamneus@alaska.edu>
2018-11-12 13:45:09 -0900

    fixed start to onl return after the node is actually running

--- boto_spawner.py
+++ boto_spawner.py
@@ -10,18 +10,21 @@ class boto_spawner(Spawner):
         super(boto_spawner, self).__init__()
         self.node_id = None
         self.aws_ec2 = boto3.resource('ec2')
-        self.running = 0
-        self.wait_on_terminate = boto3.client('ec2').get_waiter('instance_terminated')
+        self.exit_value = 0
+        self.client = boto3.client('ec2')
+        self.wait_on_terminate = self.client.get_waiter('instance_terminated')
+        self.wait_on_running = self.client.get_waiter('instance_running')
 
     @gen.coroutine
     def start(self):
-        self.running = None
+        self.exit_value = None
         # TODO create aws images for hub and nodes
         # TODO create and specify security group(probly ssh, http, https)
         # TODO specify subnet? potentially useful to limit IAM permissions for the hub
         # TODO create and specify launch template?
         node = self.aws_ec2.create_instances(ImageId='image_id', MinCount=1, MaxCount=1)
         self.node_id = node.instance_id
+        self.wait_on_running.wait(InstanceIds=[self.node_id])
         print(self.node_id)
         ip = node.public_ip_address
         # standard https port
@@ -31,8 +34,7 @@ class boto_spawner(Spawner):
     @gen.coroutine
     def stop(self, now=False):
         self.aws_ec2.instances.filter(InstanceIds=[self.node_id]).terminate()
-        exit = self.wait_on_terminate.wait(InstanceIds=[self.node_id])
-        self.running = exit
+        self.exit_value = self.wait_on_terminate.wait(InstanceIds=[self.node_id])
         # instances = self.aws_ec2.instances.filter(InstanceIds=[self.node_id], Filters=[
         #     {'Name': 'instance-state-name', 'Values': ['pending', 'running', 'shutting-down', 'stopping', 'stopped']}
         # ])
