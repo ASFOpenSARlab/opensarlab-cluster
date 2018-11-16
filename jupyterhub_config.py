67b5653ce4a0e0e4190cc87d77879e274157f133
mnamneus <mnamneus@alaska.edu>
2018-11-15 16:48:33 -0900

    removed old startup script in config file

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -370,8 +370,8 @@ from BotoSpawner import BotoSpawner
 BotoSpawner.region_name = 'us-east-1'
 # sets key for accessing nodes via ssh use key name as shown in AWS console
 BotoSpawner.ssh_key = 'mnamneus1'
-# sets shell script that runs on initial startup
-# BotoSpawner.startup_script = '#!/bin/bash\nsudo --user ubuntu bash\npip3 install -I jupyterhub\npip3 install -I notebook==5.7.0\njupyterhub-singleuser\nexit'
+# sets shell script that runs on initial startup before starting notebook server
+BotoSpawner.startup_script = ''
 # sets the AWS AMI to use when creating the node
 BotoSpawner.image_id = 'ami-052a8625391dfaa51'
 # defaults to allow all outgoing and allow all incoming ssh, http and https
