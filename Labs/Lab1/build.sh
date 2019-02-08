28fc562fddaa0a018557eb88603843d556f75138
Eric Lundell <emlundell@alaska.edu>
2019-02-07 16:32:25 -0900

    Build a better image and push to ECR

--- Labs/Lab1/build.sh
+++ Labs/Lab1/build.sh
@@ -2,7 +2,9 @@
 
 BASE_IMAGE=docker-registry.asf.alaska.edu:5000/asf-franz-labs
 
-aws s3 sync --exclude '*' --include 'ASF_MapReady_CLI.tar.gz' s3://hyp3-docker/software/ .
+#aws s3 sync --exclude '*' --include 'ASF_MapReady_CLI.tar.gz' s3://hyp3-docker/software/ .
 
-docker build -t $BASE_IMAGE:Lab1 -f Dockerfile .
-#docker push $BASE_IMAGE:Lab1
+docker build -t $BASE_IMAGE:Base -f Dockerfile --squash .
+#docker push $BASE_IMAGE:Base
+
+#docker run -it $BASE_IMAGE:Lab1 bash
