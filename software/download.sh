22d9db05c12a6e7bf0fe23f8b4cf1e3ad2695c94
Eric Lundell <emlundell@alaska.edu>
2019-03-19 13:12:21 -0800

    Add tensorflow. Rearrange download and dockerfile.

--- software/download.sh
+++ software/download.sh
@@ -1,5 +1,37 @@
 #! /bin/bash
 
-aws s3 sync --exclude '*' --include 'ASF_MapReady_CLI.tar.gz' s3://hyp3-docker/software/ . --profile=hyp3
+# Unzip all zips and tars so that we are only importing into the dockerfile plain files. This will make any future migration easier.
 
-aws s3 sync --exclude '*' --include 'ice.tar.bz2' s3://grfn-software/ . --profile=grfn
+echo "Downloading GIAnT...."
+if [ ! -d GIAnT ] ; then
+    aws s3 sync --exclude '*' --include 'GIAnT.zip' s3://asf-jupyter-software/ . --profile=jupyterhub
+    unzip GIAnT.zip
+fi
+
+echo "Downloading MapReady..."
+if [ ! -d ASF_MapReady ] ; then
+    aws s3 sync --exclude '*' --include 'ASF_MapReady_CLI.tar.gz' s3://asf-jupyter-software/ . --profile=jupyterhub
+    tar xzvf ASF_MapReady_cli.tar.gz
+fi
+
+echo "Downloading isce...."
+if [ ! -d isce ] ; then
+    aws s3 sync --exclude '*' --include 'isce.zip' s3://asf-jupyter-software/ . --profile=jupyterhub
+    unzip isce.zip
+fi
+
+echo "Downloading snap..."
+aws s3 sync --exclude '*' --include 'esa-snap_sentinel_unix_5_0.sh' s3://asf-jupyter-software/ . --profile=jupyterhub
+
+echo "Downloading other files...."
+aws s3 sync --exclude '*' --include 'focus.py' s3://asf-jupyter-software/ . --profile=jupyterhub
+
+aws s3 sync --exclude '*' --include 'prepdataxml.py' s3://asf-jupyter-software/ . --profile=jupyterhub
+
+aws s3 sync --exclude '*' --include 'topo.py' s3://asf-jupyter-software/ . --profile=jupyterhub
+
+aws s3 sync --exclude '*' --include 'unpackFrame_ALOS_raw.py' s3://asf-jupyter-software/ . --profile=jupyterhub
+
+aws s3 sync --exclude '*' --include 'snap_install.varfile' s3://asf-jupyter-software/ . --profile=jupyterhub
+
+aws s3 sync --exclude '*' --include 'gpt.vmoptions' s3://asf-jupyter-software/ . --profile=jupyterhub
