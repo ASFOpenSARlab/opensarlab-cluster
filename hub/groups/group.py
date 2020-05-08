6ec47b33b6501e31d211ace847d7f93a3be295e4
Eric Lundell <emlundell@alaska.edu>
2020-05-07 19:35:32 -0800

    Streamline db queries

--- hub/groups/group.py
+++ hub/groups/group.py
@@ -15,62 +15,73 @@ class Groups():
     def __init__(self, db=None, db_url: str = None) -> None:
 
         if db is not None:
-            self.db = db
+            self.session = db
         elif db is None and db_url is not None:
             session_factory = orm.new_session_factory(db_url)
-            self.db = session_factory()
+            self.session = session_factory()
         else:
             raise Exception("No db object or db url given.")
 
     def get_all_groups(self) -> List[orm.Group]:
-        print("get_all_groups not implemented...")
-        pass
-        #return orm.Group.all()
+        return self.session.query(orm.Group).all()
 
-    def add_group(self, group_metadata: Dict) -> None:
-        """
-        group_metadata:
-            name: name of group
-        """
-        group = orm.Group(*group_metadata)
-        self.db.add(group)
-        self.db.commit()
+    def add_group(self, group_name: str) -> None:
+        group = self.session.query(orm.Group).filter(orm.Group.name == group_name).first()
 
-    def remove_group(self, group_name: str) -> None:
-        pass
+        if group == None:
+            print(f"Group {group_name} not found.")
+            return
+
+        self.session.add(group)
+        self.session.commit()
+
+    def delete_group(self, group_name: str) -> None:
+        group = self.session.query(orm.Group).filter(orm.Group.name == group_name).first()
+
+        if group == None:
+            print(f"Group {group_name} not found.")
+            return
+
+        self.session.delete(group)
+        self.session.commit()
 
     def get_users_of_group(self, group_name: str) -> List[orm.User]:
-        group = orm.Group.find(self.db, name=group_name)
-        return [g.user for g in group]
+        group = self.session.query(orm.Group).filter(orm.Group.name == group_name).first()
+
+        if group == None:
+            print(f"Group {group_name} not found.")
+            return
+
+        return group.users
 
     def get_group_names_for_user(self, user_name: str) -> List[str]:
-        print("get_group_names_for_user not implemented...")
-        pass
-        #groups = self.get_all_groups()
-        #return [g.name for g in groups]
+        groups = self.get_all_groups()
+        return [g.name for g in groups if user_name in g.users]
 
     def add_user_to_group(self, user_name: str, group_name: str) -> None:
-        group = orm.Group.find(self.db, name=group_name)
-        user = _find_user(user_name)
+        group = self.session.query(orm.Group).filter(orm.Group.name == group_name).first()
+        user = self.session.query(orm.User).filter(orm.User.name == user_name).first()
+
+        if group == None:
+            print(f"Group {group_name} not found.")
+            return
+        if user == None:
+            print(f"User {user_name} not found.")
+            return
+
         group.users.append(user)
-        self.db.commit()
+        self.session.commit()
 
     def remove_user_from_group(self, user_name: str, group_name: str) -> None:
-        pass
+        user = self.session.query(orm.User).filter(orm.User.name == user_name).first()
+        group = self.session.query(orm.Group).filter(orm.Group.name == group_name).first()
 
-    def _find_user(self, user_name: str) -> str:
-        """Find user in database."""
-        orm_user = self.db.query(orm.User).filter(orm.User.name == user_name).first()
-        return orm_user
+        if group == None:
+            print(f"Group {group_name} not found.")
+            return
+        if user == None:
+            print(f"User {user_name} not found.")
+            return
 
-    def _add_user(self, **kwargs) -> str:
-        """Add a user to the database."""
-        orm_user = _find_user(name=kwargs.get('name'))
-        if orm_user is None:
-            orm_user = orm.User(**kwargs)
-            self.db.add(orm_user)
-        else:
-            for attr, value in kwargs.items():
-                setattr(orm_user, attr, value)
-        self.db.commit()
-        return orm_user
+        group.users.delete(user)
+        self.session.commit()
