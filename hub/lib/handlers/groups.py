6a5d50694e2e70d547a7fd116937c3d2b4688331
Eric Lundell <emlundell@alaska.edu>
2020-05-11 15:48:42 -0800

    List users as well

--- hub/lib/handlers/groups.py
+++ hub/lib/handlers/groups.py
@@ -1,5 +1,6 @@
 
 from .. import groups
+from .. import orm
 from ..utils import admin_only
 from .base import BaseHandler
 
@@ -12,9 +13,15 @@ class GroupsHandler(BaseHandler):
         g = groups.Groups(db=self.db)
         group_list = g.get_all_groups()
 
+        users = self.db.query(orm.User)
+        print(users)
+        users = [self._user_from_orm(u) for u in users]
+        print(users)
+
         html = self.render_template(
             'groups.html',
             current_user=self.current_user,
+            users=users,
             admin_access=self.settings.get('admin_access', False),
             group_list=group_list,
         )
