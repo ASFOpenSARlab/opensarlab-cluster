3e839a63c1b349c4b19b5968e0129d37bd2b6bff
Eric Lundell <emlundell@alaska.edu>
2020-05-11 21:07:17 -0800

    More clear logic in template/handler

--- hub/lib/handlers/groups.py
+++ hub/lib/handlers/groups.py
@@ -12,17 +12,21 @@ class GroupsHandler(BaseHandler):
     def get(self):
 
         g = groups.Groups(db=self.db)
-        group_list = g.get_all_groups()
+        group_list_obj = g.get_all_groups()
+        groups = {
+            'name': group_list_obj.name,
+            'members': [u.name for u in group_list_obj.users]
+        }
 
-        users = self.db.query(orm.User)
-        users = [self._user_from_orm(u) for u in users]
+        all_users_query = self.db.query(orm.User)
+        all_users = [self._user_from_orm(u) for u in all_users_query]
 
         html = self.render_template(
             'groups.html',
             current_user=self.current_user,
-            users=users,
+            groups=groups,
+            all_users=all_users,
             admin_access=self.settings.get('admin_access', False),
-            group_list=group_list,
         )
         self.finish(html)
 
@@ -37,25 +41,27 @@ class GroupsHandler(BaseHandler):
             print("Posted data: ", data)
             user_name = data['user']
             group_name = data['group']
-            is_checked = data['is_checked']
+            change_to_checked = data['change_to_checked']
 
             print(f"POST data includes: {data}")
 
             g = groups.Groups(db=self.db)
             user_names_in_group = g.get_user_names_in_group(group_name)
 
-            if is_checked == 'true':
+            print(f"Usernames in group are '{user_names_in_group}'")
+
+            if change_to_checked == 'true':
                 # if user is part of group already, skip
                 if user_name in user_names_in_group:
-                    print(f"User '{user_name}' is already added to '{group_name}'. Do nothing.")
+                    print(f"User '{user_name}' is already part of group '{group_name}'. Do nothing.")
                 else:
                     print(f"Add '{user_name}' to group '{group_name}'")
                     g.add_user_to_group(user_name, group_name)
 
             else:
-                # If user is not in group, skip
+                # If user is not already in group, skip
                 if user_name not in user_names_in_group:
-                    print(f"User '{user_name}' is already removed from '{group_name}'. Do nothing.")
+                    print(f"User '{user_name}' is already not in group '{group_name}'. Do nothing.")
                 else:
                     print(f"Remove '{user_name}' from group '{group_name}'")
                     g.remove_user_from_group(user_name, group_name)
