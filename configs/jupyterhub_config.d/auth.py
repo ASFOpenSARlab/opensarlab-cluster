ea403efda0c42e504d8e8c31874005a76c6993c9
Eric Lundell <emlundell@alaska.edu>
2021-10-20 22:24:36 -0800

    Get os from z2jh for auth

--- configs/jupyterhub_config.d/auth.py
+++ configs/jupyterhub_config.d/auth.py
@@ -1,5 +1,13 @@
+
+import os
+
+import z2jh
+
 try:
-    # c is the built-in JupyterHub config 
+    os.environ["OAUTH_DNS_NAME"] = z2jh.get_config("custom.OAUTH_DNS_NAME")
+    os.environ["OAUTH_JUPYTER_URL"] = z2jh.get_config("custom.OAUTH_JUPYTER_URL")
+    os.environ['OAUTH_POOL_NAME'] = z2jh.get_config("custom.OAUTH_POOL_NAME")
+    os.environ['REGION_NAME'] = z2jh.get_config('custom.AZ_NAME')[:-1]
 
     from generic_with_logout import GenericOAuthenticator
     c.JupyterHub.authenticator_class = GenericOAuthenticator
