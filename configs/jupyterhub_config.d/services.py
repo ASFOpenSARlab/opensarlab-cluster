56f16c79bd19bcba1db62a1d345dcd09ad73f822
Eric Lundell <emlundell@alaska.edu>
2021-10-21 00:07:13 -0800

    Reverse url name. http://name.namespace

--- configs/jupyterhub_config.d/services.py
+++ configs/jupyterhub_config.d/services.py
@@ -1,9 +1,10 @@
 
 try:
+    # Since the service is in another k8s namespace, it needs to be of the form "http://service_name.namespace"
     c.JupyterHub.services.append(
         {
             'name': 'notifications',
-            'url': 'http://services.notifications'
+            'url': 'http://notifications.services'
         }
     )
 
