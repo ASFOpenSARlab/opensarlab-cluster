d5ca2fdc47ae74661143683cd4acde1e852810ad
Eric Lundell <emlundell@alaska.edu>
2021-10-27 15:25:36 -0800

    Add hub back. All builds in cf-cluster

--- services/notifications/build.sh
+++ services/notifications/build.sh
@@ -2,20 +2,16 @@
 set -ex
 
 # ENV variables:
-# NOTIFICATIONS_FORCE_BUILD=true
 # DOCKER_REGISTRY
 
-if [ "$NOTIFICATIONS_FORCE_BUILD" = 'true' ]; then
+cp dockerfile dockerfile.build
 
-    cp dockerfile dockerfile.build
+BUILD_TAG=$(date +"%F-%H-%M-%S")
 
-    BUILD_TAG=$(date +"%F-%H-%M-%S")
+time docker build -f dockerfile.build -t $DOCKER_REPO:$BUILD_TAG -t $DOCKER_REPO:latest .
 
-    time docker build -f dockerfile.build -t $DOCKER_REGISTRY/notifications:$BUILD_TAG -t $DOCKER_REGISTRY/notifications:latest .
+# Push to registry
+docker push $DOCKER_REPO:$BUILD_TAG
+docker push $DOCKER_REPO:latest
 
-    # Push to registry
-    docker push $DOCKER_REGISTRY/notifications:$BUILD_TAG
-    docker push $DOCKER_REGISTRY/notifications:latest
-
-    echo -n ${BUILD_TAG:-latest} > get_notifications_image_build.tmp
-fi
+echo -n ${BUILD_TAG:-latest} > get_image_build.tmp
