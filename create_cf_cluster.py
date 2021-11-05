7f27ed79ab101bda788267adf24411a2fbdfabd1
Eric Lundell <emlundell@alaska.edu>
2021-11-05 12:07:13 -0800

    Node name escaped

--- create_cf_cluster.py
+++ create_cf_cluster.py
@@ -1,5 +1,6 @@
 import sys
 import pathlib
+import re
 
 import yaml
 from jinja2 import Environment, FileSystemLoader
@@ -13,6 +14,13 @@ env = Environment(
 opensarlab_yaml_path = sys.argv[1]
 template_path = sys.argv[2]
 output_config_path = sys.argv[3]
+
+# Custom filter method
+def regex_replace(s, find, replace):
+    """A non-optimal implementation of a regex filter"""
+    return re.sub(find, replace, s)
+
+env.filters['regex_replace'] = regex_replace
 
 with open(opensarlab_yaml_path, "r") as yaml_file, open(output_config_path, 'w') as output_file:
     yaml_config = yaml.safe_load(yaml_file)
