5013b61155da62251b4056af5293585803da1c52
Eric Lundell <emlundell@alaska.edu>
2021-11-12 13:15:36 -0900

    Add kubectl role Maybe this will get rid of the config forbidden error?

--- create_aws_auth.py
+++ create_aws_auth.py
@@ -14,11 +14,12 @@ opensarlab_yaml_path = sys.argv[1]
 template_path = sys.argv[2]
 output_config_path = sys.argv[3]
 
-cluster_name = sys.argv[4]
+cost_tag_value = sys.argv[4]
 region_name = sys.argv[5]
+account_id = sys.argv[6]
 
 with open(opensarlab_yaml_path, "r") as yaml_file, open(output_config_path, 'w') as output_file:
     yaml_config = yaml.safe_load(yaml_file)
 
     template = env.get_template(template_path)
-    output_file.write(template.render(opensarlab=yaml_config, cluster_name=cluster_name, region_name=region_name))
+    output_file.write(template.render(opensarlab=yaml_config, cost_tag_value=cost_tag_value, region_name=region_name, account_id=account_id ))
