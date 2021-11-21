727afbaa15061ce28e974e4cb637961a00d0dd6f
Eric Lundell <emlundell@alaska.edu>
2021-11-20 19:24:44 -0900

    Fix cf-cognito path

--- opensarlab/cf-pipeline/postbuild/create_cf_cognito.py
+++ opensarlab/cf-pipeline/postbuild/create_cf_cognito.py
@@ -13,19 +13,20 @@ env = Environment(
 
 env.filters['regex_replace'] = regex_replace
 
-def main(config):
+def main(config, output_file):
     with open(config, "r") as infile:
         yaml_config = yaml.safe_load(infile)
 
     template = env.get_template('templates/cf-cognito.yaml.jinja')
 
-    with open('lambda_handler.py', 'w') as outfile:
+    with open(output_file, 'w') as outfile:
         outfile.write(template.render(opensarlab=yaml_config))
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default=None)
+    parser.add_argument('--output_file', default=None)
     args = parser.parse_args()
 
-    main(args.config)
+    main(args.config, args.output_file)
