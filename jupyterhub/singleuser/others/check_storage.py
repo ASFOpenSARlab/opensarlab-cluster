bef986ecc2b9fb6b7ba6a4fc10d864b789e63e43
Eric Lundell <emlundell@alaska.edu>
2023-10-09 23:10:25 -0800

    jupyter-hooks => singleuser

--- jupyterhub/singleuser/others/check_storage.py
+++ jupyterhub/singleuser/others/check_storage.py
@@ -10,7 +10,7 @@ else:
 r = shutil.disk_usage('/home/jovyan/')
 
 # If less than about 1 MB
-if r.free < (1 * 1024 * 1024):
-    raise Exception(f'Out of storage space. Cannot start server. Please contact OpenSciencelab admin for assistance.')
+#if r.free < (1 * 1024 * 1024):
+#    raise Exception(f'Out of storage space. Cannot start server. Please contact OpenSciencelab admin for assistance.')
 
 print(f"Home directory storage for {user_name} is {r}")
--- singleuser_hooks/mapready_no_git.sh
+++ singleuser_hooks/mapready_no_git.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 set -ve
 
-python /etc/jupyter-hooks/resource_checks/check_storage.py $1
+python /etc/singleuser/resource_checks/check_storage.py $1
 
 # Add Path to local pip execs.
 export PATH=$HOME/.local/bin:$PATH
 
-python /etc/jupyter-hooks/scripts/pkg_clean.py
+python /etc/singleuser/scripts/pkg_clean.py
 
 python -m pip install --user nbgitpuller
 
 # copy over our version of pull.py
 # REMINDER: REMOVE IF CHANGES ARE MERGED TO NBGITPULLER
-cp /etc/jupyter-hooks/scripts/pull.py /home/jovyan/.local/lib/python3.10/site-packages/nbgitpuller/pull.py
+cp /etc/singleuser/scripts/pull.py /home/jovyan/.local/lib/python3.10/site-packages/nbgitpuller/pull.py
 
 # Disable the extension manager in Jupyterlab since server extensions are uninstallable
 # by users and non-server extension installs do not persist over server restarts
 jupyter labextension disable @jupyterlab/extensionmanager-extension
 
 mkdir -p $HOME/.ipython/profile_default/startup/
-cp /etc/jupyter-hooks/custom_magics/00-df.py $HOME/.ipython/profile_default/startup/00-df.py
+cp /etc/singleuser/custom_magics/00-df.py $HOME/.ipython/profile_default/startup/00-df.py
 
 # Update page and tree
 mv /opt/conda/lib/python3.10/site-packages/notebook/templates/tree.html /opt/conda/lib/python3.10/site-packages/notebook/templates/original_tree.html
-cp /etc/jupyter-hooks/templates/tree.html /opt/conda/lib/python3.10/site-packages/notebook/templates/tree.html
+cp /etc/singleuser/templates/tree.html /opt/conda/lib/python3.10/site-packages/notebook/templates/tree.html
 
 mv /opt/conda/lib/python3.10/site-packages/notebook/templates/page.html /opt/conda/lib/python3.10/site-packages/notebook/templates/original_page.html
-cp /etc/jupyter-hooks/templates/page.html /opt/conda/lib/python3.10/site-packages/notebook/templates/page.html
+cp /etc/singleuser/templates/page.html /opt/conda/lib/python3.10/site-packages/notebook/templates/page.html
 
 CONDARC=$HOME/.condarc
 if ! test -f "$CONDARC"; then
@@ -51,9 +51,9 @@ KERNELS=$HOME/.local/share/jupyter/kernels
 OLD_KERNELS=$HOME/.local/share/jupyter/kernels_old
 FLAG=$HOME/.jupyter/old_kernels_flag.txt
 if ! test -f "$FLAG" && test -d "$KERNELS"; then
-cp /etc/jupyter-hooks/etc/old_kernels_flag.txt $HOME/.jupyter/old_kernels_flag.txt
+cp /etc/singleuser/etc/old_kernels_flag.txt $HOME/.jupyter/old_kernels_flag.txt
 mv $KERNELS $OLD_KERNELS
-cp /etc/jupyter-hooks/etc/kernels_rename_README $OLD_KERNELS/kernels_rename_README
+cp /etc/singleuser/etc/kernels_rename_README $OLD_KERNELS/kernels_rename_README
 fi
 
 # Add a CondaKernelSpecManager section to jupyter_notebook_config.json to display nicely formatted kernel names
