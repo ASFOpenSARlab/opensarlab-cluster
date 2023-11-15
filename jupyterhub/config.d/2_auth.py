3ddfabfeb73f2e3bd3bbdbe39faa14f35178f098
Eric Lundell <emlundell@alaska.edu>
2023-11-15 10:09:42 -0900

    Cleanup sso token paths

--- jupyterhub/config.d/2_auth.py
+++ jupyterhub/config.d/2_auth.py
@@ -6,6 +6,13 @@ import z2jh
 # This try/except is needed for debugging if a problem occurs. AWS Codebuild doesn't allow for useful error messaging.
 try:
 
+    ## Set SSO token to secrets path
+    secrets_manager = boto3.client('secretsmanager', region_name=f"{z2jh.get_config('custom.AWS_REGION')}")
+    _sso_token = secrets_manager.get_secret_value(SecretId=f"sso-token/{z2jh.get_config('custom.AWS_REGION')}-{z2jh.get_config('custom.CLUSTER_NAME')}")
+    sso_token_path = os.environ.get('OPENSARLAB_SSO_TOKEN_PATH', '')
+    with open(sso_token_path, 'w') as file:
+        file.write(_sso_token['SecretString'])
+
     # If an error occurs with setting the auth but JupyterHub still starts, the dummy login will be the default. 
     # This could lead to unauthorized entry. So disable login until the last needed moment.
     print("Disabling login temporarily...")
@@ -26,13 +33,6 @@ try:
     print("All good so far. Setting login to Portal Auth...")
     from jupyterhub.portal_auth import PortalAuthenticator
     c.JupyterHub.authenticator_class = PortalAuthenticator
-
-    ## Set SSO token to secrets path
-    secrets_manager = boto3.client('secretsmanager', region_name=f"{z2jh.get_config('custom.AWS_REGION')}")
-    _sso_token = secrets_manager.get_secret_value(SecretId=f"sso-token/{z2jh.get_config('custom.AWS_REGION')}-{z2jh.get_config('custom.CLUSTER_NAME')}")
-    sso_token_path = os.environ.get('OPENSARLAB_SSO_TOKEN_PATH', '')
-    with open(sso_token_path, 'w') as file:
-        file.write(_sso_token['SecretString'])
 
 except Exception as e:
     print(e)
