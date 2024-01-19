3f75439161bb5545241925f28d4d8c6fcf687b27
Eric Lundell <emlundell@alaska.edu>
2024-01-18 16:17:19 -0900

    Better test logging

--- egress_configs/tests/start_test_pod_logs.sh
+++ egress_configs/tests/start_test_pod_logs.sh
@@ -3,14 +3,11 @@ K8S_NS=jupyter
 SE_LAB=smce-test-opensarlab
 SE_PROFILE=default
 
-if [[ -z "kubetail" ]]; then
-    wget "https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail"
+if [ ! -f "stern" ]; then
+    # https://github.com/stern/stern/releases
+    wget https://github.com/stern/stern/releases/download/v1.28.0/stern_1.28.0_linux_amd64.tar.gz
+    tar -xzf stern_1.28.0_linux_amd64.tar.gz
 fi
 
-## WIP
-./kubetail --all-namespaces -l sidecar.istio.io/inject=true --follow
-
-#SOURCE_POD=$(kubectl -n $K8S_NS get pod -l app=sleep -l se-lab=$SE_LAB -l se-profile=$SE_PROFILE -o jsonpath='{.items[0].metadata.name}')
-#kubectl -n istio-egress logs -l sidecar.istio.io/inject=true --follow
-#kubectl -n istio-system logs -l sidecar.istio.io/inject=true --follow
-#kubectl -n $K8S_NS logs -l sidecar.istio.io/inject=true --follow
+# Show logs of mulitple namespaces. The usual `kubectl logs` is rather limited
+./stern . -n jupyter,istio-egress,istio-system --tail=0
