493ff8b8158eb84f7f7979b829e37cd27885397e
Eric Lundell <emlundell@alaska.edu>
2024-02-16 13:28:49 -0900

    Add csi driver annote fixes

--- pipeline/4_deploy_and_build_jupyterhub/jupyterhub/config.d/5_pre_spawn_hook.py
+++ pipeline/4_deploy_and_build_jupyterhub/jupyterhub/config.d/5_pre_spawn_hook.py
@@ -217,8 +217,9 @@ def use_existing_volume_or_snapshot(spawner):
 
         annotations = spawn_pvc.metadata.annotations
 
-        # Explicit annote the provisioner. The CSI plugin appears to not do this properly.  
-        annotations.update({"pv.kubernetes.io/provisioned-by": "ebs.csi.aws.com"})
+        # Explicit annote the provisioner. The CSI plugin appears to not do this properly.
+        # May not be needed
+        #annotations.update({"pv.kubernetes.io/provisioned-by": "ebs.csi.aws.com"})
 
         labels = spawn_pvc.metadata.labels
 
