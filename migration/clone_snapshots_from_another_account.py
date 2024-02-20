49255f29db02d9f4869e18a84f4667645e9a8e4a
Dylan Palmieri <dgpalmieri@gmail.com>
2024-02-20 11:35:31 -0900

    fix: catch dry run exception style: apply `black` formatting
    style: apply `black` formatting
    
    squash

--- migration/clone_snapshots_from_another_account.py
+++ migration/clone_snapshots_from_another_account.py
@@ -3,7 +3,7 @@
 ######
 #
 # Clone snapshots from another account.
-# The snapshots must be visible from the account. This might require changing permissions on the snapshot. 
+# The snapshots must be visible from the account. This might require changing permissions on the snapshot.
 # Meant to be run in cloudshell of the particular region/account where the proper environment varibles are set.
 #
 ######
@@ -12,96 +12,81 @@ import time
 
 import boto3
 
-def main(args):
 
+def main(args):
     # Initialize Boto3
     session = boto3.Session()
-    ec2 = session.client('ec2')
+    ec2 = session.client("ec2")
 
-    if args['specific_user_claim']:
+    if args["specific_user_claim"]:
         # Get specfic snapshot of a particular user (if it exists)
         response = ec2.describe_snapshots(
             Filters=[
                 {
-                    'Name': f"tag:kubernetes.io/cluster/{args['new_cluster_name']}",
-                    'Values': ['owned']
+                    "Name": f"tag:kubernetes.io/cluster/{args['new_cluster_name']}",
+                    "Values": ["owned"],
                 },
+                {"Name": "status", "Values": ["completed", "pending", "error"]},
+                {"Name": f"tag:from-{args['old_cluster_name']}", "Values": "true"},
                 {
-                    'Name': 'status',
-                    'Values': ['completed', 'pending', 'error']
+                    "Name": f"tag:kubernetes.io/created-for/pvc/name",
+                    "Values": [f"{args['specific_user_claim']}"],
                 },
-                {
-                    'Name': f"tag:from-{args['old_cluster_name']}",
-                    'Values': 'true'
-                },
-                {
-                    'Name': f"tag:kubernetes.io/created-for/pvc/name",
-                    'Values': [f"{args['specific_user_claim']}"]
-                }
             ],
-            OwnerIds=['self']
+            OwnerIds=["self"],
         )
 
     else:
         # Get list of all snapshots belonging to particular cluster
         response = ec2.describe_snapshots(
-                Filters=[
-                    {
-                        'Name': f"tag:kubernetes.io/cluster/{args['new_cluster_name']}",
-                        'Values': ['owned']
-                    },
-                    {
-                        'Name': 'status',
-                        'Values': ['completed', 'pending', 'error']
-                    },
-                    {
-                        'Name': f"tag:from-{args['old_cluster_name']}",
-                        'Values': 'true'
-                    },
-                    {
-                        'Name': f"tag:kubernetes.io/created-for/pvc/name",
-                        'Values': f"args['specific_user_claim']"
-                    }
-                ],
-                OwnerIds=['self']
-            )
-    
-    snapshots = response['Snapshots']
+            Filters=[
+                {
+                    "Name": f"tag:kubernetes.io/cluster/{args['new_cluster_name']}",
+                    "Values": ["owned"],
+                },
+                {"Name": "status", "Values": ["completed", "pending", "error"]},
+                {"Name": f"tag:from-{args['old_cluster_name']}", "Values": "true"},
+                {
+                    "Name": f"tag:kubernetes.io/created-for/pvc/name",
+                    "Values": f"args['specific_user_claim']",
+                },
+            ],
+            OwnerIds=["self"],
+        )
+
+    snapshots = response["Snapshots"]
 
     if len(snapshots) == 0:
         print("No snapshots found")
 
     if len(snapshots) > 0:
         for snap in snapshots:
-
             print(f"Cloning volume: {snap}]\n\n")
 
             try:
                 response = ec2.copy_snapshot(
-                    SourceRegion=args['old_region_name'],
-                    SourceSnapshotId=snap['snapshotId'],
+                    SourceRegion=args["old_region_name"],
+                    SourceSnapshotId=snap["snapshotId"],
                     TagSpecifications=[
-                        {
-                            'ResourceType': 'snapshot',
-                            'Tags': snap['Tags']
-                        },
+                        {"ResourceType": "snapshot", "Tags": snap["Tags"]},
                     ],
-                    DryRun=True
+                    DryRun=True,
                 )
                 print(f"Snapshot '{snap['snapshotId']}' copied to new account.")
 
-            except ec2.exceptions.ClientError as e:
+            except ec2.exceptions.ClientError:
                 print("Too many pending snapshots. Wait for 1 minute and continue.")
                 time.sleep(60)
-            
+            except ec2.meta.client.exceptions.DryRunOperation:
+                print("Dry Run Operation has all necessary permissions")
+
 
 if __name__ == "__main__":
-
     args = {
-        'old_cluster_name': 'smce-test-cluster',
-        'new_cluster_name': 'smce-prod-cluster',
-        'old_region_name': 'us-west-2',
-        'specific_user_claim': 'claim-emlundell'
+        "old_cluster_name": "smce-test-cluster",
+        "new_cluster_name": "smce-prod-cluster",
+        "old_region_name": "us-west-2",
+        "specific_user_claim": "claim-emlundell",
     }
 
     main(args)
--- migration/take_snapshots_of_volumes_for_another_account.py
+++ migration/take_snapshots_of_volumes_for_another_account.py
@@ -8,26 +8,28 @@
 ######
 
 import boto3
-def main(args):
 
+import time
+
+
+def main(args):
     # Initialize Boto3
     session = boto3.Session()
-    ec2 = session.client('ec2')
+    ec2 = session.client("ec2")
 
-    if args['specific_user_claim']:
+    if args["specific_user_claim"]:
         # Get specfic volume of a particular user (if it exists)
         volumes = ec2.describe_volumes(
             Filters=[
                 {
-                    'Name': f"tag:kubernetes.io/cluster/{args['old_cluster_name']}",
-                    'Values': ['owned']
+                    "Name": f"tag:kubernetes.io/cluster/{args['old_cluster_name']}",
+                    "Values": ["owned"],
                 },
                 {
-                    'Name': f"tag:kubernetes.io/created-for/pvc/name",
-                    'Values': [f"{args['specific_user_claim']}"]
-                }
+                    "Name": f"tag:kubernetes.io/created-for/pvc/name",
+                    "Values": [f"{args['specific_user_claim']}"],
+                },
             ],
-
         )
 
     else:
@@ -35,104 +37,105 @@ def main(args):
         volumes = ec2.describe_volumes(
             Filters=[
                 {
-                    'Name': f"tag:kubernetes.io/cluster/{args['old_cluster_name']}",
-                    'Values': ['owned']
+                    "Name": f"tag:kubernetes.io/cluster/{args['old_cluster_name']}",
+                    "Values": ["owned"],
                 },
             ]
         )
 
-    volumes = volumes['Volumes']
+    volumes = volumes["Volumes"]
 
     if len(volumes) == 0:
         print("No volumes found")
 
     if len(volumes) > 0:
-
         # Take snapshots of each volume. Make sure the tags are copied over.
         for vol in volumes:
-
             print(f"Converting tags for volume: {vol}]\n\n")
 
-            old_tags = vol['Tags']
+            old_tags = vol["Tags"]
             # tags = [{'Key': 'string','Value': 'string'},]
             new_tags = []
 
             for tag in old_tags:
-                if tag['Key'] in [
-                    'server-stop-time',
-                    'snapshot-delete-time',
-                    'ebs.csi.aws.com/cluster',
-                    'kubernetes.io/created-for/pvc/name',
-                    'volume-delete-time',
-                    'server-start-time'
+                if tag["Key"] in [
+                    "server-stop-time",
+                    "snapshot-delete-time",
+                    "ebs.csi.aws.com/cluster",
+                    "kubernetes.io/created-for/pvc/name",
+                    "volume-delete-time",
+                    "server-start-time",
                 ]:
                     new_tags.append(tag)
 
-                elif tag['Key'] == 'Name':
-                    new_tags.append({
-                        'Key': 'Name', 'Value': f"migrated-{tag['Value']}"
-                    })
+                elif tag["Key"] == "Name":
+                    new_tags.append(
+                        {"Key": "Name", "Value": f"migrated-{tag['Value']}"}
+                    )
 
-                elif tag['Key'] == 'osl-billing':
-                    new_tags.append({
-                        'Key': 'osl-billing', 'Value': args['new_billing_value']
-                    })
+                elif tag["Key"] == "osl-billing":
+                    new_tags.append(
+                        {"Key": "osl-billing", "Value": args["new_billing_value"]}
+                    )
 
-                elif tag['Key'] == f"kubernetes.io/cluster/{args['old_cluster_name']}":
-                    new_tags.append({
-                        'Key': f"kubernetes.io/cluster/{args['new_cluster_name']}", 'Value': 'owned'
-                    })
+                elif tag["Key"] == f"kubernetes.io/cluster/{args['old_cluster_name']}":
+                    new_tags.append(
+                        {
+                            "Key": f"kubernetes.io/cluster/{args['new_cluster_name']}",
+                            "Value": "owned",
+                        }
+                    )
 
-                elif tag['Key'] == f'KubernetesCluster':
-                    new_tags.append({
-                        'Key': f'KubernetesCluster', 'Value': args['new_cluster_name']
-                    })
+                elif tag["Key"] == f"KubernetesCluster":
+                    new_tags.append(
+                        {"Key": f"KubernetesCluster", "Value": args["new_cluster_name"]}
+                    )
 
-            new_tags.append({
-                'Key': f"from-{args['old_cluster_name']}", 'Value': 'true'
-            })
+            new_tags.append(
+                {"Key": f"from-{args['old_cluster_name']}", "Value": "true"}
+            )
 
             print(f"Creating snapshot from volume: {vol['VolumeId']}\n")
 
             response = ec2.create_snapshot(
-                VolumeId=vol['VolumeId'],
+                VolumeId=vol["VolumeId"],
                 TagSpecifications=[
-                    {
-                        'ResourceType': 'snapshot',
-                        'Tags': new_tags
-                    },
+                    {"ResourceType": "snapshot", "Tags": new_tags},
                 ],
-                DryRun=False
+                DryRun=False,
             )
 
-            # TODO: Handle error
-            # botocore.exceptions.ClientError: An error occurred (SnapshotCreationPerVolumeRateExceeded) when calling the CreateSnapshot operation: The maximum per volume CreateSnapshot request rate has been exceeded. Use an increasing or variable sleep interval between requests.
-            # https://docs.aws.amazon.com/AWSEC2/latest/APIReference/errors-overview.html
-
-
-            snapshot_id = response['SnapshotId']
+            snapshot_id = response["SnapshotId"]
 
             # Modify permissions of snapshot and ADD NEW ACCOUNT NUMBER, as needed
-            if args['new_account_id']:
-                print(f"Modifiying permissions for snapshot {snapshot_id} from volume {vol['VolumeId']}")
-                response = ec2.modify_snapshot_attribute(
-                    Attribute='createVolumePermission',
-                    OperationType='add',
-                    SnapshotId=snapshot_id,
-                    UserIds=[
-                        args['new_account_id'],
-                    ],
-                    DryRun=False
+            if args["new_account_id"]:
+                print(
+                    f"Modifiying permissions for snapshot {snapshot_id} from volume {vol['VolumeId']}"
                 )
+                try:
+                    response = ec2.modify_snapshot_attribute(
+                        Attribute="createVolumePermission",
+                        OperationType="add",
+                        SnapshotId=snapshot_id,
+                        UserIds=[
+                            args["new_account_id"],
+                        ],
+                        DryRun=False,
+                    )
+                except ec2.exceptions.ClientError:
+                    print("Too many pending snapshots. Wait for 1 minute and continue.")
+                    time.sleep(60)
+                except ec2.meta.client.exceptions.DryRunOperation:
+                    print("Dry Run Operation has all necessary permissions")
+
 
 if __name__ == "__main__":
-
     args = {
-        'old_cluster_name': 'smce-test-cluster',
-        'new_cluster_name': 'smce-prod-cluster',
-        'new_billing_value': 'smce-prod',
-        'new_account_id': '381492216607',
-        'specific_user_claim': 'claim-emlundell'
+        "old_cluster_name": "smce-test-cluster",
+        "new_cluster_name": "smce-prod-cluster",
+        "new_billing_value": "smce-prod",
+        "new_account_id": "381492216607",
+        "specific_user_claim": "claim-emlundell",
     }
 
     main(args)
